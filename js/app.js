710bf032371d734033208e5b7f63b9200032a258
Mohammed Sufyan <sufyan297@gmail.com>
2018-09-08 11:55:24 +0530

    SendMail added.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+
+  var isMailSent = getParameterByName('mail_sent');
+  if (isMailSent == 1) {
+    $.notify("Your request has been successfully submitted.");
+  }
 
   $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
+    loop: true,
+    margin: 10,
+    nav: true,
     stagePadding: 50,
     autoplay: true,
     autoplayTimeout: 10 * 1000,
     navText: ['<img src="./images/chevron-left.svg">', '<img src="./images/chevron-right.svg">'],
-    responsive:{
-      0:{
-        items:1,
+    responsive: {
+      0: {
+        items: 1,
         nav: false,
         stagePadding: 20
       },
-      600:{
-        items:1
+      600: {
+        items: 1
       },
-      1000:{
-        items:1
+      1000: {
+        items: 1
       }
     }
   });
@@ -31,7 +37,7 @@ $(document).ready(function() {
     blipMarker(idx);
     setInterval(() => {
       blipMarker(idx);
-      idx ++;
+      idx++;
       if (idx > 2) {
         idx = 0;
       }
@@ -40,7 +46,7 @@ $(document).ready(function() {
 
 
   //Smooth Scroll
-  $('.footer-navigation-title').on('click', function(event) {
+  $('.footer-navigation-title').on('click', function (event) {
     // // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
@@ -60,61 +66,61 @@ $(document).ready(function() {
       }
     } // End if
   });
-  
+
   /*
   * Show More/Less
   */
 
-    // Configure/customize these variables.
-    var showChar = 210;  // How many characters are shown by default
-    var ellipsestext = "...";
-    var moretext = "Show more";
-    var lesstext = "Show less";
-    
+  // Configure/customize these variables.
+  var showChar = 210;  // How many characters are shown by default
+  var ellipsestext = "...";
+  var moretext = "Show more";
+  var lesstext = "Show less";
 
-    $('.more').each(function() {
-        var content = $(this).html();
- 
-        if(content.length > showChar) {
- 
-            var c = content.substr(0, showChar);
-            var h = content.substr(showChar, content.length - showChar);
- 
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
- 
-            $(this).html(html);
-        }
- 
-    });
-    $(".morelink").click(function(){
-        if($(this).hasClass("less")) {
-            $(this).removeClass("less");
-            $(this).html(moretext);
-        } else {
-            $(this).addClass("less");
-            $(this).html(lesstext);
-        }
-        $(this).parent().prev().toggle();
-        $(this).prev().toggle();
-        return false;
-    });
+
+  $('.more').each(function () {
+    var content = $(this).html();
+
+    if (content.length > showChar) {
+
+      var c = content.substr(0, showChar);
+      var h = content.substr(showChar, content.length - showChar);
+
+      var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+      $(this).html(html);
+    }
+
+  });
+  $(".morelink").click(function () {
+    if ($(this).hasClass("less")) {
+      $(this).removeClass("less");
+      $(this).html(moretext);
+    } else {
+      $(this).addClass("less");
+      $(this).html(lesstext);
+    }
+    $(this).parent().prev().toggle();
+    $(this).prev().toggle();
+    return false;
+  });
 
 
 });
 
 function scrollToAnimate(hash, milliseconds = 800) {
- // Using jQuery's animate() method to add smooth page scroll
+  // Using jQuery's animate() method to add smooth page scroll
   // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
   $('html, body').animate({
     scrollTop: $(hash).offset().top
-  }, milliseconds, function(){
+  }, milliseconds, function () {
     // Add hash (#) to URL when done scrolling (default click behavior)
     window.location.hash = hash;
   });
 }
 
 function blipMarker(idx = 0) {
-  switch(idx) {
+  switch (idx) {
     case 0: $('.map-marker-2').css('background-image', 'url(./images/Ascendtis_Location_green_0.png)'); break;
     case 1: $('.map-marker-2').css('background-image', 'url(./images/Ascendtis_Location_green_1.png)'); break;
     case 2: $('.map-marker-2').css('background-image', 'url(./images/Ascendtis_Location_green_2.png)'); break;
@@ -123,8 +129,18 @@ function blipMarker(idx = 0) {
 }
 
 function goToGoogleMap() {
-  window.open("https://goo.gl/maps/1VDjYoSD54t","_blank");
+  window.open("https://goo.gl/maps/1VDjYoSD54t", "_blank");
 }
 function goToUrl(url = '#') {
-  window.open(url,"_blank");
+  window.open(url, "_blank");
+}
+
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
---
+++ send_mail.php
@@ -0,0 +1,33 @@
+<?php
+require 'vendor/autoload.php';
+$client = new \GuzzleHttp\Client();
+//variables
+$apikey = getenv("ELASTICEMAIL_API_KEY");
+$email = $_POST['email'];
+$name = $_POST['name'];
+$interested_in = $_POST['interested_in'];
+$message = $_POST['message'];
+$subject = "Contact Us Request - Ascendtis";
+$to = "support@ascendtis.com";
+$from = "noreply@ascendtis.com";
+$fromName = "Ascendtis Support";
+$bodyHtml = "
+<h1> Contact Us </h1>
+<br/>
+<br/>
+<b>Name: </b> $name<br/>
+<b>Email: </b> $email<br/>
+<b>Interested In: </b> $interested_in <br/>
+<b>Message: </b> $message<br/>
+";
+
+$endpoint = "https://api.elasticemail.com/v2/";
+$url = $endpoint."email/send?apikey=".$apikey."&bodyHtml=".$bodyHtml."&from=".$from."&fromName=".$fromName."&to=".$to."&subject=".$subject;
+// Send an asynchronous request.
+$request = new \GuzzleHttp\Psr7\Request('GET', $url);
+$promise = $client->sendAsync($request)->then(function ($response) {
+    echo $response->getBody();
+    header('Location: https://ascendtis.com/?mail_sent=1');
+});
+$promise->wait();
+?>
